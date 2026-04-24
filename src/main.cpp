e355a91271e85e0297ad5f3aaa96849dfe7c48eb
Niko Huuskonen <59277828+N1kO23@users.noreply.github.com>
2026-04-24 17:01:15 +0300

    Add backlight dimming and rotary encoder support

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,20 @@
 #define SDA_PIN 6
 #define SCL_PIN 7
 
+#define ENC_CLK 2
+#define ENC_DT 3
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 Si5351 si5351;
 
 bool si5351Connected = false;
+
+const uint64_t stepSize = 1000ULL; // 1 kHz steps
+int lastCLKState;
+
+unsigned long lastActivityTime = 0;
+const unsigned long backlightTimeout = 10000; // 10 seconds
+bool backlightOn = true;
 
 // TODO: Add ways to change the carrier frequency and to turn the transmitter on/off.
 uint64_t carrierFreq = 6000000ULL; // 6 MHz
@@ -45,17 +55,23 @@ void showErrorScreen()
   lcd.print("Retrying...");
 }
 
-void showFrequencyScreen()
+void updateDisplay()
 {
   lcd.clear();
+
   lcd.setCursor(0, 0);
-  lcd.print("Carrier:");
+  lcd.print("Carrier Freq:");
 
   lcd.setCursor(0, 1);
 
   float mhz = carrierFreq / 1000000.0;
   lcd.print(mhz, 3);
   lcd.print(" MHz");
+}
+
+void updateFrequency()
+{
+  si5351.set_freq(carrierFreq * 100ULL, SI5351_CLK0);
 }
 
 bool initSi5351()
@@ -73,11 +89,18 @@ void setup()
 {
   Wire.begin(SDA_PIN, SCL_PIN);
 
+  pinMode(ENC_CLK, INPUT_PULLUP);
+  pinMode(ENC_DT, INPUT_PULLUP);
+
   lcd.init();
   lcd.backlight();
 
+  lastActivityTime = millis();
+
   showBootScreen();
   delay(2000);
+
+  lastCLKState = digitalRead(ENC_CLK);
 }
 
 void loop()
@@ -87,12 +110,52 @@ void loop()
     if (initSi5351())
     {
       si5351Connected = true;
-      showFrequencyScreen();
+      updateDisplay();
     }
     else
     {
       showErrorScreen();
       delay(2000);
+      return;
     }
+  }
+
+  int currentCLKState = digitalRead(ENC_CLK);
+
+  if (currentCLKState != lastCLKState && currentCLKState == LOW)
+  {
+
+    // Wake LCD if asleep
+    if (!backlightOn)
+    {
+      lcd.backlight();
+      backlightOn = true;
+    }
+
+    lastActivityTime = millis();
+
+    if (digitalRead(ENC_DT) != currentCLKState)
+    {
+      carrierFreq += stepSize;
+    }
+    else
+    {
+      if (carrierFreq > stepSize)
+      {
+        carrierFreq -= stepSize;
+      }
+    }
+
+    updateFrequency();
+    updateDisplay();
+  }
+
+  lastCLKState = currentCLKState;
+
+  // Turn off backlight after timeout
+  if (backlightOn && (millis() - lastActivityTime > backlightTimeout))
+  {
+    lcd.noBacklight();
+    backlightOn = false;
   }
 }
